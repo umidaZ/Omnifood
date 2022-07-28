993e7101943d08fc8494829c80797315eabafd71
umidaZ <umidaismoilova0217@gmail.com>
2022-07-28 16:14:21 +0500

    commit

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+//// Year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+//// Navigation
 const btnNav = document.querySelector(".btn-mobile-nav");
 const headerNav = document.querySelector(".header");
 
 btnNav.addEventListener ("click", function(){
   headerNav.classList.toggle("nav-open")
 })
+
+//// Smooth
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 
