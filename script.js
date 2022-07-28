e4a05737f4dde0d9eb80729e1c7104956bf23006
umidaZ <umidaismoilova0217@gmail.com>
2022-07-28 15:53:48 +0500

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerNav = document.querySelector(".header");
+
+btnNav.addEventListener ("click", function(){
+  headerNav.classList.toggle("nav-open")
+})
+
